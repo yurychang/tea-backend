c0c6b2b3cc4f3a261657e1b8af7ca5f9f6f16b57
Liou <a8532582@gmail.com>
2020-03-13 14:43:30 +0800

    addNodemonAndTestDB

--- app.js
+++ app.js
@@ -9,38 +9,62 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-const cors = request('cros')
+const db = require('./src/_connect_db');
+const bodyParser = require('body-parser');
+const uelencodeParrser = bodyParser.urlencoded({ extended: false });
+
+const cors = require('cors');
 var whitelist = ['http://localhost:3000',
   undefined,
   'http://192.168.1.27:3000',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
+  'http://localhost:3939',
+  'http://127.0.0.1:3939',
+  'http://192.168.1.27:3939'
 ];
+
 var corsOptions = {
   credentials: true,
-  origin: function (origin, callback){
-    console.log('origin:'+origin);
-    if(whitelist.indexOf(origin) !== -1){
-      callback(null,true)
-    }else{
+  origin: function (origin, callback) {
+    console.log('origin:' + origin);
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      console.log("11111")
       callback(new Error('非白名單的網域'))
     }
   }
 };
+app.use(uelencodeParrser);
+app.use(bodyParser.json());
+
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cros(corsOptions));
+//app.use(cors(corsOptions));
+app.use('*', cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.get('/try-db', (req, res) => {
+  const sql = "SELECT `vendorPassword` from `vendordata`";
+  db.query(sql, (error, result, fields) => {
+    if (!error) {
+      res.json(result);
+    } else {
+      res.end(error);
+    }
+    console.log(result);
+  });
+});
 app.use('/users', usersRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
