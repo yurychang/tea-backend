f182e080ded2119bb0bae7c1f3ce1a332f4d9d1b
peter <peter01twn@gmail.com>
2020-03-24 22:32:32 +0800

    merge

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -13,6 +12,7 @@ const vendorRouter = require("./routes/vendor");
 const zonesRouter = require("./routes/zones");
 const companysRouter = require("./routes/companys");
 const productRouter = require("./routes/product");
+// const memberRouter = require('./routes/member')
 
 const urlencodeParser = bodyParser.urlencoded({ extended: false });
 const multer = require("multer");
@@ -21,28 +21,6 @@ const upload = multer({ dest: "tmp_uploads" });
 const cors = require("cors");
 var whitelist = [
   "http://localhost:3000",
-=======
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-
-const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
-// const eventsRouter = require('./routes/events');
-// const vendorRouter = require('./routes/vendor');
-// const zonesRouter = require('./routes/zones');
-// const companysRouter = require('./routes/companys');
-const memberRouter = require('./routes/member')
-
-
-const urlencodeParser = bodyParser.urlencoded({ extended: false });
-
-const cors = require('cors');
-var whitelist = ['http://localhost:3000',
->>>>>>> 34be5c5f4147e63941342deb22c53cf6abc181ed
   undefined,
   "http://192.168.1.27:3000",
   "http://127.0.0.1:3000",
@@ -67,10 +45,6 @@ const corsOptions = {
 
 const app = express();
 
-<<<<<<< HEAD
-const db = require("./migrations/_connect_db");
-=======
->>>>>>> 34be5c5f4147e63941342deb22c53cf6abc181ed
 app.use(urlencodeParser);
 app.use(bodyParser.json());
 
@@ -84,7 +58,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-<<<<<<< HEAD
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/users", usersRouter);
@@ -93,18 +66,8 @@ app.use("/vendor", vendorRouter);
 app.use("/zones", zonesRouter);
 app.use("/companys", companysRouter);
 app.use("/product", productRouter);
+// app.use('/member', memberRouter);
 app.use("/", indexRouter);
-=======
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/users', usersRouter);
-// app.use('/events', eventsRouter);
-// app.use('/vendor', vendorRouter);
-// app.use('/zones', zonesRouter);
-// app.use('/companys', companysRouter);
-app.use('/member', memberRouter);
-app.use('/', indexRouter);
->>>>>>> 34be5c5f4147e63941342deb22c53cf6abc181ed
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
