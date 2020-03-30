559cabb4f9be0c1f10a106a2ac0991128b5434c6
Liou <a8532582@gmail.com>
2020-03-30 13:48:49 +0800

    addproductAPI

--- routes/vendor.js
+++ routes/vendor.js
@@ -73,7 +73,7 @@ router.post('/vendorsignup', (req, res) => {
     const sql = "INSERT INTO vendordata(vendorAccount,vendorPassword,vendorEmail,vendorPhone) VALUE(?,?,?,?) ";
     db.query(sql, [req.body.vendorAccount, req.body.vendorPassword, req.body.vendorEmail, req.body.vendorPhone], (error, results, fields) => {
       if (error) { throw error }
-      console.log('signupresults',results)
+      console.log('signupresults', results)
       if (results.affectedRows === 1) {
         data.success = true;
         data.message.type = 'primary';
@@ -145,17 +145,20 @@ router.get('/getvendordata', vendorVerification, (req, res) => {
 
 //更新廠商資料API
 
-router.post('/updatedata',vendorVerification ,upload.single('vendorImg'), (req, res) => {
+router.post('/updatedata', vendorVerification, upload.single('vendorImg'), (req, res) => {
+  let zoneId = req.body.vendorZone.slice(0, 1)
   let venderObj = {
     vendorName: req.body.vendorName,
     vendorEmail: req.body.vendorEmail,
     vendorPhone: req.body.vendorPhone,
+    zoneId: zoneId,
     vendorZone: req.body.vendorZone,
     vendorAddress: req.body.vendorAddress,
   }
   try {
     const data = { success: false, message: { type: 'danger', text: '', url: '', imgmsg: '' } };
     if (req.file) {
+      console.log('path',req.file)
       switch (req.file.mimetype) {
         case 'image/jpeg':
         case 'image/png':
@@ -243,7 +246,6 @@ router.post('/updateabout', upload.single('vendorBanner'), (req, res) => {
     db.query(sql, [venderObj, req.body.localId], (error, results, fields) => {
       if (error) { throw error }
       if (results.length === 1) {
-        // console.log('results',results)
         data.success = true;
         data.message.type = 'primary';
         data.message.text = '更新成功'
@@ -261,15 +263,12 @@ router.post('/updateabout', upload.single('vendorBanner'), (req, res) => {
 
 
 //取得廠商訂單API(列表)
-router.get('/getvendorderlist',vendorVerification, (req, res) => {
+router.get('/getvendorderlist', vendorVerification, (req, res) => {
   const sql = "SELECT `id`,`memberId`, `vendorId`, `totalPrice`,`orderId` FROM `orderdata` WHERE vendorId=?";
   let id = req.session.vendorOnlyId
-  console.log(id)
   db.query(sql, id, (error, results, fields) => {
     if (error) throw error
-    console.log(results)
     res.json(results);
-
   });
   return
 });
@@ -279,9 +278,8 @@ router.get('/getvendorderlist',vendorVerification, (req, res) => {
 
 //取得廠商訂單API(單筆詳細)
 router.get('/getvendororder/:orderid', (req, res) => {
-  const sql = "SELECT orderdata.id , orderdata.memberId ,orderdetail.productName ,orderdetail.productPrice ,orderdetail.productAmount FROM orderdata INNER JOIN orderdetail ON orderdata.id=orderdetail.orderId WHERE id=?";
+  const sql = "SELECT orderdata.id , orderdata.memberId ,orderdetail.productName ,orderdetail.productPrice ,orderdetail.productAmount FROM orderdata INNER JOIN orderdetail ON orderdata.id=orderdetail.orderId WHERE orderdata.id=?";
   let orderid = req.params.orderid
-  console.log('orderid', orderid)
   db.query(sql, orderid, (error, results, fields) => {
     if (error) throw error
     console.log(results)
@@ -292,10 +290,22 @@ router.get('/getvendororder/:orderid', (req, res) => {
 });
 
 //取得廠商商品API(列表)
-router.get('/getvendorproductlist',vendorVerification, (req, res) => {
+router.get('/getvendorproductlist', vendorVerification, (req, res) => {
   const sql = "SELECT `id`, `title`, `tag`, `classIfy`, `price`, `unit`, `sTime`, `idVendor`, `feaTure`, `img` FROM `commodity` WHERE `idVendor`=?";
   let id = req.session.vendorOnlyId
-  console.log(id)
+  db.query(sql, id, (error, results, fields) => {
+    if (error) throw error
+    console.log(results)
+    res.json(results);
+
+  });
+  return
+});
+
+//取得廠商商品API(前端列表)
+router.get('/getvendorproductlistuseparam/:vendorid',  (req, res) => {
+  const sql = "SELECT `id`, `title`, `tag`, `classIfy`, `price`, `unit`, `sTime`, `idVendor`, `feaTure`, `img` FROM `commodity` WHERE `idVendor`=?";
+  let id = req.params.vendorid
   db.query(sql, id, (error, results, fields) => {
     if (error) throw error
     console.log(results)
